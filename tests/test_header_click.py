c4aa8f640ae076147f2e3c1a3695dc5346135741
Alex ER <100380496+kolik1983@users.noreply.github.com>
2022-08-12 13:27:51 +0300

    Update test_header_click.py

--- tests/test_header_click.py
+++ tests/test_header_click.py
@@ -1,1 +1,23 @@
+# How to run:
+#  1) Download geko driver for Chrome here:
+#     https://chromedriver.chromium.org/downloads
+#  2) Install all requirements:
+#     pip install -r requirements.txt
+#  3) Run tests:
+#     python3 -m pytest -v --driver Chrome --driver-path ~/chrome tests/*
 
+
+
+
+import pytest
+from pages.main_page import MainPage
+
+
+def test_check_logo_refresh(web_browser):
+    page = MainPage(web_browser)
+    page.logo.click()
+    
+    assert 
+
+  
+  

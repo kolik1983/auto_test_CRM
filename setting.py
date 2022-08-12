ecf47119acbdcf71aad2fc7c670e8ebc00b73cef
Alex ER <100380496+kolik1983@users.noreply.github.com>
2022-08-12 14:13:19 +0300

    Update setting.py

--- setting.py
+++ setting.py
@@ -1,1 +1,11 @@
 main_url = 'https://minifreemarket.com/'
+auth_url = 'https://minifreemarket.com/cabinet'
+correct_login = 'erigov@mail.ru'
+correct_pass = 'fktrctq83'
+incorrect_login = 'qwe123@#$'
+incorrect_pass = '12345678'
+rus_login = 'абвгд@'
+more_255_pass = '1' * 256
+null_email = ''
+find_bar_eng = 'necrons'
+necron_url = 'https://minifreemarket.com/catalog?fg_text=necrons&'

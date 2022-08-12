38de11c27d1c7d84fc669799407535836578ce32
Alex ER <100380496+kolik1983@users.noreply.github.com>
2022-08-12 12:28:05 +0300

    Update main_page.py

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,1 +1,15 @@
+import pytest
+from pages import MainPage
+
+
+def test_check_main_search(web_browser):
+    """ Make sure main search works fine. """
+
+    page = MainPage(web_browser)
+
+    page.search = 'iPhone 12'
+    page.search_run_button.click()
+
+    # Verify that user can see the list of products:
+    assert page.products_titles.count() == 48
 

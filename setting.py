a279aaf8932500b8caef2a353534569d654493e3
Alex ER <100380496+kolik1983@users.noreply.github.com>
2022-08-15 12:03:55 +0300

    Update setting.py

--- setting.py
+++ setting.py
@@ -9,3 +9,4 @@ more_255_pass = '1' * 256
 null_email = ''
 find_bar_eng = 'necrons'
 necron_url = 'https://minifreemarket.com/catalog?fg_text=necrons&'
+shops_url = 'https://minifreemarket.com/shops'

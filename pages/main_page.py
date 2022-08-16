a3d0bf36160b523ddaf930877396e720b22ea5e9
Alex ER <100380496+kolik1983@users.noreply.github.com>
2022-08-16 10:25:55 +0300

    Update main_page.py

--- pages/main_page.py
+++ pages/main_page.py
@@ -21,20 +21,31 @@ class MainPage(WebPage):
     FAV_BTN = WebElement(xpath='//a[contains(text(), "Избранное")]')
     # Rus lang swich button
     RUS_LANG = WebElement(xpath='//a[contains(text(), "RUS")]')
-    # Rus lang switch button
+    # Eng lang switch button
     ENG_LANG = WebElement(xpath='//a[contains(text(), "RUS")]')
-    
+    # Switch rus lang
     LOGO_RUS = WebElement(xpath='//div[contains(text(), "Язык")]')
+    # Switch eng lang
     LOGO_ENG = WebElement(xpath='//div[contains(text(), "Language")]')
+    # Currency in rub
     RUB_BTN = WebElement(xpath='//a[contains(text(), "RUB")]')
+     # Currency in usd
     USD_BTN = WebElement(xpath='//a[contains(text(), "USD")]')
+     # Price in specified currency
     USD_TXT = WebElement(xpath='//div[@class="price"][1]')
+     # Currency in eur
     EUR_BTN = WebElement(xpath='//a[contains(text(), "EUR")]')
+    # Price in specified currency
     EUR_TXT = WebElement(xpath='//div[@class="price"][1]')
+    # Busket button
     BST_BTN = WebElement(xpath='//a[@class="basket"]')
+    # Item button
     CAT_BTN = WebElement(xpath='//div[@class="item"]')
+    # Category button
     CAT_MNU = WebElement(xpath='//div[@class="cat_menu"]')
+    # Shop button
     SHP_BTN = WebElement(xpath='//div[@class="item shops_btn"]')
+    # Busket button
     SEL_BTN = WebElement(xpath='//div[@class="item adadd"]')
     USR_BTN = WebElement(xpath='//a[@class="user"]')
     AUTH_BTN = WebElement(xpath='//a[@class="login"]')

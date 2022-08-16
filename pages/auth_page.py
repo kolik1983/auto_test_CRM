241c424fdd1b549429008e39588197c4248ffe4d
Alex ER <100380496+kolik1983@users.noreply.github.com>
2022-08-16 11:59:07 +0300

    Update auth_page.py

--- pages/auth_page.py
+++ pages/auth_page.py
@@ -13,13 +13,21 @@ class MainPage(WebPage):
 
         super().__init__(web_driver, url)
 
-    #login string
+    # login string
     CAB_REG = WebElement(xpath='//a[contains(text(), "учетную запись")]')
+    # auth bar
     AUTH_B = WebElement(xpath='//input[@name="login_"]')
+    # password bar
     PASS_B = WebElement(xpath='//input[@name="password_"]')
+    # login button
     LOG_BTN = WebElement(xpath='//a[@class="btn submit"]')
+    # registration button
     REG_HR = WebElement(xpath='//form/a[contains(text(), "Регистрация")]')
+    # recovery passw button
     FGT_HR = WebElement(xpath='//a[@class="fr"]')
+    # VK button reg
     VK_BTN = WebElement(xpath='//a[@class="soc_btn vk"]')
+    # facebook button reg
     FB_BTN = WebElement(xpath='//a[@class="soc_btn fb"]')
+    # yandex button reg
     YA_BTN = WebElement(xpath='//a[@class="soc_btn ya"]')

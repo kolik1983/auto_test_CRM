420f0b75dd0b809da7c8686dfcdc34db8193bf86
Alex ER <100380496+kolik1983@users.noreply.github.com>
2022-08-16 11:20:13 +0300

    Update auth_page.py

--- pages/auth_page.py
+++ pages/auth_page.py
@@ -1,1 +1,17 @@
+import os
 
+from pages.base import WebPage
+from pages.elements import WebElement
+from pages.elements import ManyWebElements
+
+
+class MainPage(WebPage):
+
+    def __init__(self, web_driver, url=''):
+        if not url:
+            url = os.getenv("MAIN_URL") or 'https://minifreemarket.com/cabinet'
+
+        super().__init__(web_driver, url)
+
+    # Main LOGO.
+    LOGO_BTN = WebElement(xpath='//a[@class="logo"]')

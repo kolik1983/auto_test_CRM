ee7154aa2960af0258b9b73a71c1abefa6a1457a
Alex ER <100380496+kolik1983@users.noreply.github.com>
2022-08-16 13:34:55 +0300

    Update test_header_click.py

--- tests/test_header_click.py
+++ tests/test_header_click.py
@@ -26,6 +26,37 @@ def test_02_check_category_menu(web_browser):
     page.CAT_BTN.click()
     assert page.CAT_MNU.is_visible()
     
+def test_03_shop_page_visible(web_browser):
+    """Проверка загрузки страницы продавцов после нажатия на кнопку продавцы"""
+    page = MainPage(web_browser)
+    page.SHP_BTN.click()
+    assert page.get_current_url() == Conf.shops_url
+    
+def test_04_sell_page_visible(web_browser):
+    """Проверка загрузки страницы подачи объявления о продаже после нажания на кнопку"""
+    page = MainPage(web_browser)
+    page.SEL_BTN.click()
+    assert page.get_current_url() == Conf.sel_url
+    
+def test_05_change_lang_eng(web_browser):
+    """Проверка изменения языка на сайте при смене языка на англ"""
+    page = MainPage(web_browser)
+    page.ENG_LANG.click()
+    assert "Language" in page.LOGO_ENG.text() 
+    
+def test_06_change_lang_rus(web_browser):
+    """Проверка изменения языка на сайте при смене языка на рус"""
+    page = MainPage(web_browser)
+    page.RUS_LANG.click()
+    assert "Язык" in page.LOGO_RUS.text()
+    
+def test_07_
+    
+    
+    
+    
+    
+    
     
     
     

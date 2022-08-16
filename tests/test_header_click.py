73be08d8d48b7438b671f26c8dbb84a24ff20ee4
Alex ER <100380496+kolik1983@users.noreply.github.com>
2022-08-16 12:14:19 +0300

    Update test_header_click.py

--- tests/test_header_click.py
+++ tests/test_header_click.py
@@ -14,8 +14,11 @@ from pages.main_page import MainPage
 from setting import Conf
 
 def test_01_check_logo_refresh(web_browser):
+    """Проверка возврата на главную странице при нажатие на логотип"""
     page = MainPage(web_browser)
     page.HELP_BTN.click()
     page.LOGO_BTN.click()
     assert page.get_current_url() == Conf.main_url
+    
+def test_02_    
     

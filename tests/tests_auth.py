e5c779ebd097c54897cc6b9e35488817376c91d2
Alex ER <100380496+kolik1983@users.noreply.github.com>
2022-08-18 17:13:49 +0300

    Update tests_auth.py

--- tests/tests_auth.py
+++ tests/tests_auth.py
@@ -13,6 +13,37 @@ from pages.auth_page import AuthPage
 from setting import Conf
 
 def test_01_login_valid_pass_name(web_browser):
-  """"""
+  """Проверка входа в систему с валидными данными"""
+  page = AuthPage(web_browser)
+  page.AUTH_B.send_keys(Conf.correct_login)
+  page.PASS_B.send_keys(Conf.correct_pass)
+  page.LOG_BTN.click()
+  assert page.INF_USR.is_visible and 'Kolik' in page.NIC_USR.text()
+  
+def test_02_login_with_null_name(web_browser):
+  """Попытка войти личный кабинет с пустым полем Ник"""
+  page = AuthPage(web_browser)
+  page.AUTH_B.send_keys(Conf.null_str)
+  page.PASS_B.send_keys(Conf.correct_pass)
+  page.LOG_BTN.click()
+  assert page.ERR_MSG.is_visible
+ 
+def test_03_login_with_null_pass(web_browser):
+  """Попытка войти личный кабинет с пустым полем пароля"""
+  page = AuthPage(web_browser)
+  page.AUTH_B.send_keys(Conf.correct_login)
+  page.PASS_B.send_keys(Conf.null_str)
+  page.LOG_BTN.click()
+  assert page.ERR_MSG.is_visible
+  
+def test_04_login_with_null_both(web_browser):
+  """Попытка войти личный кабинет с пустым полем пароля и ника"""
+  page = AuthPage(web_browser)
+  page.AUTH_B.send_keys(Conf.null_str)
+  page.PASS_B.send_keys(Conf.null_str)
+  page.LOG_BTN.click()
+  assert page.ERR_MSG.is_visible
+  
+  
 
 

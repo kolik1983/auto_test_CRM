0cd8b6269a3a5f7a63a8728b2206e6abe58ce956
Alex ER <100380496+kolik1983@users.noreply.github.com>
2022-08-18 16:43:19 +0300

    Update tests_auth.py

--- tests/tests_auth.py
+++ tests/tests_auth.py
@@ -1,1 +1,18 @@
+# How to run:
+#  1) Download geko driver for Chrome here:
+#     https://chromedriver.chromium.org/downloads
+#  2) Install all requirements:
+#     pip install -r requirements.txt
+#  3) Run tests:
+# python -m pytest -v --driver Chrome --driver-path C:\Users\erigo\PycharmProjects\auto_test_CRM\tests\test_header_click.py
+# python -m pytest -v --driver Chrome --driver-path c:/driver/chromedriver.exe C:\Users\erigo\PycharmProjects\auto_test_CRM\tests\test_header_click.py
+
+import pytest
+from pages.main_page import MainPage
+from pages.auth_page import AuthPage
+from setting import Conf
+
+def test_01_login_valid_pass_name(web_browser):
+  """"""
+
 

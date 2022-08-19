28a1d50ac3be046655e259a9b0c820a5e7d991b7
Alex ER <100380496+kolik1983@users.noreply.github.com>
2022-08-19 13:35:50 +0300

    Update tests_auth.py

--- tests/tests_auth.py
+++ tests/tests_auth.py
@@ -188,10 +188,16 @@ def test_23_registration_link(web_browser):
   assert page.get_current_url() == Conf.reg_url
   
 def test_24_forgoth_pass_link(web_browser):
-  """Проерка перехода по ссылке забыли пароль? на соответсвующую страницу"""
+  """Проерка перехода по ссылке забыли пароль? на страницу регистрации"""
   page.AuthPage(web_browser)
   page.FGT_HR.click()
   assert page.get_current_url() == Conf.fgt_url
+  
+def test_25_user_registration_link(web_browser):
+  """Проверка перехода п ссылке учетрая запись на страницу регистрации"""
+  page.AuthPage(web_browser)
+  page.CAB_REG.click()
+  assert page.get_current_url() == Conf.reg_url
   
   
 

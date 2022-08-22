5b50ec82fd4164dc6413a8f07070c4e82dca7899
Alex ER <100380496+kolik1983@users.noreply.github.com>
2022-08-22 16:00:32 +0300

    Update tests_auth.py

--- tests/tests_auth.py
+++ tests/tests_auth.py
@@ -4,8 +4,8 @@
 #  2) Install all requirements:
 #     pip install -r requirements.txt
 #  3) Run tests:
-# python -m pytest -v --driver Chrome --driver-path C:\Users\erigo\PycharmProjects\auto_test_CRM\tests\test_header_click.py
-# python -m pytest -v --driver Chrome --driver-path c:/driver/chromedriver.exe C:\Users\erigo\PycharmProjects\auto_test_CRM\tests\test_header_click.py
+# python -m pytest -v --driver Chrome --driver-path C:\Users\erigo\PycharmProjects\auto_test_CRM\tests\test_auth.py
+# python -m pytest -v --driver Chrome --driver-path c:/driver/chromedriver.exe C:\Users\erigo\PycharmProjects\auto_test_CRM\tests\test_auth.py
 
 import pytest
 from pages.main_page import MainPage

2108fefa90e0f59625a1a7e12d81a10675dc0a91
Alex ER <100380496+kolik1983@users.noreply.github.com>
2022-08-23 17:51:37 +0300

    Update tests_auth.py

--- tests/tests_auth.py
+++ tests/tests_auth.py
@@ -18,8 +18,8 @@ def test_01_login_valid_pass_name(web_browser):
   page.AUTH_B.send_keys(Conf.correct_login)
   page.PASS_B.send_keys(Conf.correct_pass)
   page.LOG_BTN.click()
-  assert page.INF_USR.is_visible and 'Kolik' in page.NIC_USR.text()
-  
+  assert page.INF_USR.is_visible and page.NIC_USR.is_visible()
+
 def test_02_login_with_null_name(web_browser):
   """Попытка войти личный кабинет с пустым полем Ник"""
   page = AuthPage(web_browser)
@@ -27,7 +27,7 @@ def test_02_login_with_null_name(web_browser):
   page.PASS_B.send_keys(Conf.correct_pass)
   page.LOG_BTN.click()
   assert page.ERR_MSG.is_visible()
- 
+
 def test_03_login_with_null_pass(web_browser):
   """Попытка войти личный кабинет с пустым полем пароля"""
   page = AuthPage(web_browser)
@@ -35,7 +35,7 @@ def test_03_login_with_null_pass(web_browser):
   page.PASS_B.send_keys(Conf.null_str)
   page.LOG_BTN.click()
   assert page.ERR_MSG.is_visible()
-  
+
 def test_04_login_with_null_both(web_browser):
   """Попытка войти личный кабинет с пустым полем пароля и ника"""
   page = AuthPage(web_browser)
@@ -48,15 +48,17 @@ def test_04_login_with_null_both(web_browser):
 def test_05_login_with_nonvalid_name(web_browser):
   """Проверка входа с невалидным именем"""
   page = AuthPage(web_browser)
+  page.wait_page_loaded()
   page.AUTH_B.send_keys(Conf.incorrect_login)
   page.PASS_B.send_keys(Conf.correct_pass)
   page.LOG_BTN.click()
   assert page.ERR_MSG.is_visible()
 
 
-def test_06_login_with_nonvalid_password(web_browser):
+def test_06_login_with_not_valid_password(web_browser):
   """Проверка входа с невалидным паролем"""
   page = AuthPage(web_browser)
+  page.wait_page_loaded()
   page.AUTH_B.send_keys(Conf.correct_login)
   page.PASS_B.send_keys(Conf.digit)
   page.LOG_BTN.click()
@@ -64,7 +66,7 @@ def test_06_login_with_nonvalid_password(web_browser):
 
 
 def test_07_login_with_space_after_valid_name(web_browser):
-  """Проверка входа валидным логином и пробелом в конце"""
+  """Проверка входа валидным логином и пробелом в конце.  Этот тест нашел баг, фактически сайт принимает такое имя, а должен выдавать ошибку"""
   page = AuthPage(web_browser)
   page.AUTH_B.send_keys(Conf.key_space_login)
   page.PASS_B.send_keys(Conf.correct_pass)
@@ -75,6 +77,7 @@ def test_07_login_with_space_after_valid_name(web_browser):
 def test_08_login_with_space_after_valid_password(web_browser):
   """Проверка входа валидным паролем и пробелом в конце"""
   page = AuthPage(web_browser)
+  page.wait_page_loaded()
   page.AUTH_B.send_keys(Conf.correct_login)
   page.PASS_B.send_keys(Conf.key_space_passw)
   page.LOG_BTN.click()
@@ -82,44 +85,48 @@ def test_08_login_with_space_after_valid_password(web_browser):
 
 
 def test_09_special_symbol(web_browser):
-  """Проверка входа при вводе спец. символов в полк пароля"""
+  """Проверка входа при вводе спец. символов в поле пароля"""
   page = AuthPage(web_browser)
+  page.wait_page_loaded()
   page.AUTH_B.send_keys(Conf.correct_login)
   page.PASS_B.send_keys(Conf.spec_sym)
   page.LOG_BTN.click()
   assert page.ERR_MSG.is_visible()
 
 
-def test_10_254_symbols_password(web_browser):
-  """Проверка входа при вооде 254 символов в поле пароля"""
+def test_10_251_symbols_password(web_browser):
+  """Проверка входа при вводе 251 символа в поле пароля"""
   page = AuthPage(web_browser)
   page.AUTH_B.send_keys(Conf.correct_login)
-  page.PASS_B.send_keys(Conf.number_254_pass)
+  page.PASS_B.send_keys(Conf.number_251_pass)
   page.LOG_BTN.click()
   assert page.ERR_MSG.is_visible()
 
 
-def test_11_255_symbols_password(web_browser):
-  """Проверка входа при вводе 255 символов в поле пароля"""
+def test_11_252_symbols_password(web_browser):
+  """Проверка входа при вводе 252 символов в поле пароля"""
   page = AuthPage(web_browser)
+  page.wait_page_loaded()
   page.AUTH_B.send_keys(Conf.correct_login)
-  page.PASS_B.send_keys(Conf.number_255_pass)
+  page.PASS_B.send_keys(Conf.number_252_pass)
   page.LOG_BTN.click()
   assert page.ERR_MSG.is_visible()
 
 
-def test_12_256_symbols_password(web_browser):
-  """Проверка входа при вводе больше 255 символов в поле пароля"""
+def test_12_253_symbols_password(web_browser):
+  """Проверка входа при вводе больше 252 символов в поле пароля"""
   page = AuthPage(web_browser)
+  page.wait_page_loaded()
   page.AUTH_B.send_keys(Conf.correct_login)
-  page.PASS_B.send_keys(Conf.more_255_pass)
+  page.PASS_B.send_keys(Conf.more_252_pass)
   page.LOG_BTN.click()
   assert page.ERR_MSG.is_visible()
 
 
 def test_13_rus_symbols_password(web_browser):
-  """Проверка входа при вводе руссских букв в поле пароля"""
+  """Проверка входа при вводе русских букв в поле пароля"""
   page = AuthPage(web_browser)
+  page.wait_page_loaded()
   page.AUTH_B.send_keys(Conf.correct_login)
   page.PASS_B.send_keys(Conf.rus_login)
   page.LOG_BTN.click()
@@ -127,107 +134,106 @@ def test_13_rus_symbols_password(web_browser):
 
 
 def test_14_rus_symbols_name(web_browser):
-  """Проверка входа при вводе руссских букв в поле имени"""
+  """Проверка входа при вводе русских букв в поле имени"""
   page = AuthPage(web_browser)
+  page.wait_page_loaded()
   page.AUTH_B.send_keys(Conf.rus_login)
   page.PASS_B.send_keys(Conf.correct_pass)
   page.LOG_BTN.click()
   assert page.ERR_MSG.is_visible()
 
 
-def test_15_254_symbols_name(web_browser):
-  """Проверка входа при вводе 254 символов в поле имени"""
+def test_15_251_symbols_name(web_browser):
+  """Проверка входа при вводе 251 символа в поле имени"""
   page = AuthPage(web_browser)
-  page.AUTH_B.send_keys(Conf.number_254_pass)
+  page.wait_page_loaded()
+  page.AUTH_B.send_keys(Conf.number_251_pass)
   page.PASS_B.send_keys(Conf.correct_pass)
   page.LOG_BTN.click()
   assert page.ERR_MSG.is_visible()
 
 
-def test_16_255_symbols_name(web_browser):
-  """Проверка входа при вводе 255 символов в поле имени"""
+def test_16_252_symbols_name(web_browser):
+  """Проверка входа при вводе 252 символов в поле имени"""
   page = AuthPage(web_browser)
-  page.AUTH_B.send_keys(Conf.number_255_pass)
+  page.wait_page_loaded()
+  page.AUTH_B.send_keys(Conf.number_252_pass)
   page.PASS_B.send_keys(Conf.correct_pass)
   page.LOG_BTN.click()
   assert page.ERR_MSG.is_visible()
 
 
-def test_17_256_symbols_name(web_browser):
-  """Проверка входа при вводе больше 255 символов в поле имени"""
+def test_17_253_symbols_name(web_browser):
+  """Проверка входа при вводе больше 252 символов в поле имени"""
   page = AuthPage(web_browser)
-  page.AUTH_B.send_keys(Conf.more_255_pass)
-  page.PASS_B.send_keys(Conf.correct_pass)
-  page.LOG_BTN.click()
-
-
-def test_18_login_with_space_after_valid_name(web_browser):
-  """Проверка входа валидным именем и пробелом в конце"""
-  page = AuthPage(web_browser)
-  page.AUTH_B.send_keys(Conf.key_space_login)
+  page.wait_page_loaded()
+  page.AUTH_B.send_keys(Conf.more_252_pass)
   page.PASS_B.send_keys(Conf.correct_pass)
   page.LOG_BTN.click()
   assert page.ERR_MSG.is_visible()
 
 
-def test_19_special_symbol(web_browser):
+def test_18_special_symbol(web_browser):
   """Проверка входа при вводе спец. символов в поле имени"""
   page = AuthPage(web_browser)
+  page.wait_page_loaded()
   page.AUTH_B.send_keys(Conf.spec_sym)
   page.PASS_B.send_keys(Conf.correct_pass)
   page.LOG_BTN.click()
   assert page.ERR_MSG.is_visible()
 
 
-def test_20_VK_button(web_browser):
+def test_19_VK_button(web_browser):
   """Проверка перехода на страницу авторизации через VK"""
   page = AuthPage(web_browser)
   page.VK_BTN.click()
   assert page.get_current_url() == Conf.vk_url
 
 
-def test_21_FB_button(web_browser):
+def test_20_FB_button(web_browser):
   """Проверка перехода на страницу авторизации через FB"""
   page = AuthPage(web_browser)
   page.FB_BTN.click()
   assert page.get_current_url() == Conf.fb_url
 
 
-def test_22_YA_button(web_browser):
+def test_21_YA_button(web_browser):
   """Проверка перехода на страницу авторизации через YA"""
   page = AuthPage(web_browser)
   page.YA_BTN.click()
   assert page.get_current_url() == Conf.ya_url
 
 
-def test_23_registration_link(web_browser):
-  """Проерка перехода по ссылке регистрация на соответсвующую страницу"""
+def test_22_registration_link(web_browser):
+  """Проверка перехода по ссылке регистрация на соответсвующую страницу"""
   page = AuthPage(web_browser)
   page.REG_HR.click()
   assert page.get_current_url() == Conf.reg_url
 
 
-def test_24_forgoth_pass_link(web_browser):
-  """Проерка перехода по ссылке забыли пароль? на страницу регистрации"""
+def test_23_forgoth_pass_link(web_browser):
+  """Проверка перехода по ссылке забыли пароль? на страницу регистрации"""
   page = AuthPage(web_browser)
   page.FGT_HR.click()
   assert page.get_current_url() == Conf.fgt_url
 
 
-def test_25_user_registration_link(web_browser):
+def test_24_user_registration_link(web_browser):
   """Проверка перехода по ссылке учетная запись на страницу регистрации"""
   page = AuthPage(web_browser)
   page.CAB_REG.click()
   assert page.get_current_url() == Conf.reg_url
-  
-  def test_26_login_icon_with_authorization(web_browser):
+
+def test_25_login_icon_with_authorization(web_browser):
   """Проверка выпадающего меню профиля при нажатии на кнопку профиля при аутентификации"""
   page = AuthPage(web_browser)
+  page.wait_page_loaded()
   page.AUTH_B.send_keys(Conf.correct_login)
   page.PASS_B.send_keys(Conf.correct_pass)
   page.LOG_BTN.click()
   page.USR_BTN.click()
   assert page.PERS_BOX.is_presented()
+
 
 
   
